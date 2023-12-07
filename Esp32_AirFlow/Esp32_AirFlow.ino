66cf07dfbf764de85a4b680ff2178f9343e177de
Kenan Alic <kenan.alic@edu.fit.ba>
2023-12-07 18:27:09 +0100

    Interrupt added

--- Esp32_AirFlow/Esp32_AirFlow.ino
+++ Esp32_AirFlow/Esp32_AirFlow.ino
@@ -1,44 +1,41 @@
 #define fanPin 14    
-#define MQ135 34
+#define MQ135 34  // Assuming MQ135 is connected to Analog pin A0
 
-int sensorVal = 0;
 const int gasThreshold = 1500;
-const int samplingInterval = 1000;  
-const int fanSpinDuration = 10000;  
+const int fanSpinDuration = 10000;
+int sensorVal=0;
+volatile bool fanSpinActive = false;
+volatile unsigned long fanStartTime = 0;  
+hw_timer_t *timer = NULL;
 
-unsigned long previousMillis = 0;
-unsigned long fanStartTime = 0; 
-bool fanSpinActive = false;
+void IRAM_ATTR onTimer() {
+  sensorVal = analogRead(MQ135);
+  if (sensorVal > gasThreshold) {
+    digitalWrite(fanPin, HIGH);
+    fanSpinActive = true;
+    fanStartTime = millis(); 
+  }
+}
 
 void setup() {
   pinMode(fanPin, OUTPUT);
   pinMode(MQ135, INPUT);
   Serial.begin(115200);
-}
 
-void fanOff() {
-  digitalWrite(fanPin, LOW);
-  fanSpinActive = false;
+  
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000, true); 
+  timerAlarmEnable(timer);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= samplingInterval) {
-    previousMillis = currentMillis;
-
-    sensorVal = analogRead(MQ135);
-
-    if (sensorVal > gasThreshold) {
-      digitalWrite(fanPin, HIGH);
-      fanSpinActive = true;
-      fanStartTime = currentMillis;  
+ 
+  if (fanSpinActive) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - fanStartTime >= fanSpinDuration) {
+      digitalWrite(fanPin, LOW);
+      fanSpinActive = false;
     }
   }
-
-  if (fanSpinActive && currentMillis - fanStartTime >= fanSpinDuration) {
-    fanOff();  
-  }
-
-  delay(100); 
 }
